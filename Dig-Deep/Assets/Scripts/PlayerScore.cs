340511f25b1a8d4cac95fcaca2c043588c7924a3
jungo <ga45jas@mytum.de>
2017-06-09 23:19:53 +0200

    Not much changed

---
+++ Dig-Deep/Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScore : MonoBehaviour {
+    private int score = 0;
+    private int multiplyer = 1;
+    public float speed = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Pro Frame oder pro Sekunde? Frames sind hier eigentlich besser
+        score += (int)((speed) * multiplyer);
+        Debug.Log(score);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            speed++;
+        }
+	}
+
+    public void setMultiplyer(int materialID)
+    {
+        //Setze Multiplyer abhängig von Material
+    }
+
+}
