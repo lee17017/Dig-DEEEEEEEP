6342a4c58fff990da6d83a1141a0f0e2e876f0a8
Robin <Ostner_Robin@web.de>
2017-06-11 01:40:26 +0200

    Optimized some things

--- Dig-Deep/Assets/Scripts/PlayerAnimation.cs
+++ Dig-Deep/Assets/Scripts/PlayerAnimation.cs
@@ -91,6 +91,42 @@ public class PlayerAnimation : MonoBehaviour {
                 break;
         }
 	}
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        pSpeed = GameManager.current.baseSpeed + GetComponent<Player>().correctPerSecond * GameManager.current.speedEffect;
+
+        transform.Translate(Vector3.down * Time.deltaTime * pSpeed);
+
+        if ((mod((int)transform.position.y, 32) <= 16 && !switching))
+        {
+            switchNext();
+        }
+
+        if (mod((int)(transform.position.y), 32) <= 1)
+        {
+            switchField();
+        }
+
+        if (mod((int)(transform.position.y), 32) >= 31 && !switching2)
+        {
+            switchField2();
+        }
+
+
+
+        if (switching && (mod((int)transform.position.y, 32) >= 20))
+        {
+            switching = false;
+        }
+
+        if (switching2 && (mod((int)transform.position.y, 32) >= 20))
+        {
+            switching2 = false;
+        }
+    }
 
     void switchField()
     {
@@ -118,39 +154,5 @@ public class PlayerAnimation : MonoBehaviour {
     int mod(int a, int b)
     {
         return (a % b + b) % b;
-    }
-    // Update is called once per frame
-    void Update () {
-
-        pSpeed = GameManager.current.baseSpeed + GetComponent<Player>().spinsPerSecond * GameManager.current.speedEffect;
-       
-        transform.Translate(Vector3.down * Time.deltaTime * pSpeed);
-
-        if ((mod((int)transform.position.y, 32) <= 16 && !switching))
-        {
-            switchNext();
-        }
-
-        if (mod((int)(transform.position.y), 32) <= 1)
-        {
-           switchField();
-        }
-
-        if (mod((int)(transform.position.y), 32) >= 31 && !switching2)
-        {
-            switchField2();
-        }
-
-
-
-        if (switching && (mod((int)transform.position.y, 32) >= 20))
-            {
-            switching = false;
-            }
-
-        if (switching2 && (mod((int)transform.position.y, 32) >= 20))
-        {
-            switching2 = false;
-        }
     }
 }
