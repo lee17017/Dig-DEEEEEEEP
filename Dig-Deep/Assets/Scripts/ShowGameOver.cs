efb067cb529973da774fa0fb1bbcf7fd15c6b5d7
Florian Sprang <florian.sprang@tum.de>
2017-06-10 22:40:44 +0200

    added game over

---
+++ Dig-Deep/Assets/Scripts/ShowGameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowGameOver : MonoBehaviour {
+
+    public Text gOver;
+
+    private bool display = false;
+
+    private void Awake()
+    {
+        gOver.gameObject.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update () {
+        if (!GameManager.current.run && !display)
+        {
+            gOver.gameObject.SetActive(true);
+            display = true;
+        }
+	}
+}
