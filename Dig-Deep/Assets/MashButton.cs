0824ac1d2e536c440cb8197a443c532b834db7ed
Robin <Ostner_Robin@web.de>
2017-06-11 06:39:43 +0200

    Warnings

---
+++ Dig-Deep/Assets/MashButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MashButton : MonoBehaviour
+{
+
+    public int player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("joystick " + player + " button 0"))
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.5f);
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+        }
+    }
+}
